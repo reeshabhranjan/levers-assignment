8eb35d1569335ab0dd507990cf7e545fd14a5384
Reeshabh <reeshabhkumarranjan@gmail.com>
2024-04-16 23:39:59 +0530

    Fix a bug in database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./sql_app.db'
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URI")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args=dict(check_same_thread=False)
