d021ca7a0f1b06520471421d3d9342bde8b3ddca
Reeshabh <reeshabhkumarranjan@gmail.com>
2024-04-16 23:28:44 +0530

    Fix DATABASE_URI env-var related bugs

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 
 alembic upgrade head
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn app.main:app --env-file /app/.env --host 0.0.0.0 --port 8000 --reload
