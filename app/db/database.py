83d65535b7b4363b33aced8ad65edf4829d7451a
Reeshabh <reeshabhkumarranjan@gmail.com>
2024-04-16 23:45:53 +0530

    Fix bugs

--- app/db/database.py
+++ app/db/database.py
@@ -6,9 +6,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URI")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args=dict(check_same_thread=False)
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
