9868d9adb66413cdc3ead840c6fa691f04ccdc3f
Reeshabh <reeshabhkumarranjan@gmail.com>
2024-04-18 02:48:53 +0530

    Make error messages more clear

--- app/bills/validators.py
+++ app/bills/validators.py
@@ -13,12 +13,12 @@ def validate_create_bill(bill: schemas.BillCreateWithSubBills, db: Session):
 
     sum_bill_amount_total = sum(sub_bill.amount for sub_bill in bill.sub_bills)
     if sum_bill_amount_total > bill.total:
-        raise HTTPException(status_code=400, detail=f'Sum of the sub_bill is greater than the total amount of the '
-                                                    'bill! (total: {bill.total}, sum: {sum_bill_amount_total})')
+        raise HTTPException(status_code=400, detail='Sum of the sub_bill is greater than the total amount of the '
+                                                    f'bill! (total: {bill.total}, sum: {sum_bill_amount_total})')
 
     if sum_bill_amount_total < bill.total:
-        raise HTTPException(status_code=400, detail=f'Sum of the sub_bill is less than the total amount of the bill! ('
-                                                    'total: {bill.total}, sum: {sum_bill_amount_total})')
+        raise HTTPException(status_code=400, detail='Sum of the sub_bill is less than the total amount of the bill! ('
+                                                    f'total: {bill.total}, sum: {sum_bill_amount_total})')
 
     # keeping it at the end to avoid unnecessary db queries
     if crud.sub_bills_exists(db, references):
